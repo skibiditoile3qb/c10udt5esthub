c1d48ebd0cbb1d08a4c41eb196f2c6cd58fc09a9
skibiditoile3qb <jijojak@usd437.net>
2025-06-28 07:26:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ 
   storage: storage,
   limits: {
-    fileSize: 50 * 1024 * 1024 // 50MB limit
+    fileSize: 100 * 1024 * 1024 // 100MB limit
   }
 });
 
